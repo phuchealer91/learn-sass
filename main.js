2d8f789e295ce68b7c476fed0f42b6963abaf8ff
phuchealer91 <pepegvcn2@gmail.com>
2019-08-20 10:53:11 +0700

    langding-page p1

---
+++ main.js
@@ -0,0 +1,16 @@
+let demo = document.getElementById('demo');
+// demo.addEventListener('click',function(event){
+//     alert('Ban da nhap vao link !');
+//     event.stopPropagation();
+// });
+function my(event){
+    alert('ban da nhap vao link !');
+    event.stopPropagation();
+}
+function myFunction(){
+    alert('Ban da nhap vao phan tu p !');
+}
+// demo.onclick(function(event){
+//         alert('ban nhap vao link !');
+//         event.stopPropagation();
+// });
